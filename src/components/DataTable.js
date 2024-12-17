39ee34d57e94f7abec8dbaa963f1a30b9ff97ac5
payamfara <payamfara7@gmail.com>
2024-12-17 19:55:17 +0330

    some changes

--- src/components/DataTable.js
+++ src/components/DataTable.js
@@ -16,47 +16,7 @@ const DataTable = ({ data, columns }) => {
       data: data,
       columns: columns,
       destroy: true,
-      pagingType: "simple_numbers",
-      dom: '<"dataTables_wrapper dt-bootstrap5 no-footer"<"col-12 card-header d-flex justify-content-between border-top rounded-0 flex-wrap py-3"f<"d-flex flex-wrap gap-3"l<"custom-actions inner-h-100"><"custom-actions-hidden d-none"B><"add-product inner-h-100">>>rt<"row justify-content-between p-2"<"col-sm-12 col-md-6"i><"col-sm-12 col-md-6"p>>>',
-      // dom: '<"d-flex justify-content-between"Bf>rtip', 
-      buttons: ["copy", "excel", "csv", "print"],
-      initComplete: function () {
-        document.querySelector('.dt-search').classList.add('form-floating');
-        document.querySelector('.dt-length > select').classList.add('h-100');
-        document.querySelector('.dt-info').classList.add('dataTables_info');
-        document.querySelector('.dt-paging').classList.add('dataTables_paginate');
-
-        // swap input & label position
-        const parent = document.querySelector('.dt-search'); 
-        const input = parent.querySelector('input');
-        const label = parent.querySelector('label');
-        if (input && label) {
-          parent.insertBefore(input, label); 
-        }
-
-        const btnCopy = document.querySelector(".custom-actions-hidden .buttons-copy");
-        const btnCsv = document.querySelector(".custom-actions-hidden .buttons-csv");
-        const btnPrint = document.querySelector(".custom-actions-hidden .buttons-print");
-        const items = [
-          { 'label': 'چاپ', 'icon': <IconPrinter size={16} />, onClick: () => btnPrint.click() },
-          { 'label': 'Csv', 'icon': <IconFile size={16} />, onClick: () => btnCsv.click() },
-          { 'label': 'کپی', 'icon': <IconCopy size={16} />, onClick: () => btnCopy.click() },
-        ]
-        replaceComponent(
-          ".custom-actions",
-          <CustomDropdown icon={<IconDownload size={16} />} title={'گرفتن خروجی'} items={items} />
-        );
-        replaceComponent(
-          ".add-product",
-          <RippleButton
-            className={`d-flex align-items-center gap-1 z-1 btn btn-primary p-1 px-4`}
-            title="Add Product"
-          >
-            <IconPlus size={16} />
-            افزودن محصول
-          </RippleButton>
-        );
-      },
+      dom: 'rt',
       language: {
         info: "نمایش _START_ تا _END_ از _TOTAL_ ورودی",
         infoEmpty: "هیچ داده‌ای موجود نیست",
